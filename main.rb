353108239767737ec5bd866cb02509b5a1b4cacc
Jaime Iniesta <jaimeiniesta@gmail.com>
2015-02-18 10:50:04 +0100

    adds custom user agent

--- main.rb
+++ main.rb
@@ -27,9 +27,18 @@ end
 
 get '/scrape' do
   if params[:url]
-    @page = MetaInspector.new(params[:url], warn_level: :store, :connection_timeout => 5, :read_timeout => 5)
+    @page = MetaInspector.new(params[:url],
+                              :warn_level => :store,
+                              :connection_timeout => 5, :read_timeout => 5,
+                              :headers => { 'User-Agent' => user_agent })
     erb :scrape
   else
     redirect "/"
   end
+end
+
+private
+
+def user_agent
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36"
 end
